2f94c8a1b69ee79fb6b195da96c5a174768ebf6e
RickaPrincy <rckprincy@gmail.com>
2025-05-15 15:26:43 +0300

    chore: project generated with templi

---
+++ include/ussd/ussd.hpp
@@ -0,0 +1,5 @@
+#pragma once
+
+namespace ussd{
+    int sum(int a, int b);
+}
---
+++ sources/ussd.cpp
@@ -0,0 +1,6 @@
+#include <ussd/ussd.hpp>
+#include <ussd/ussdConfig.hpp>
+
+int ussd::sum(int a, int b){
+    return a + b;
+}
